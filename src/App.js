53a944dcb9627fe5552bfabc8f5dddd8b439d1ed
morar03 <morarcristiantraian@gmail.com>
2021-01-04 19:33:20 +0200

    fortoday

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
 
-function App() {
+import Particles from 'react-particles-js';
+
+const particlesOption ={
+  particles: {
+      number:{
+        value:60,
+        density:{
+          enable:true,
+          value_area: 800
+        },
+      }
+      }
+    }
+  
+  
+
+
+function App()  {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className="particles"
+        params={particlesOption} 
+        />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+    {/* <FaceRecognition /> */}
     </div>
   );
 }
